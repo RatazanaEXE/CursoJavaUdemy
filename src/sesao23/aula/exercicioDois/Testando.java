ddfa74dddacd78bed0e8134a8c9b6e2f5078f794
RatazanaEXE <claudia.r.carvalho2015@gmail.com>
2026-03-23 20:43:30 -0300

    Generics

---
+++ src/sesao23/aula/exercicioDois/Testando.java
@@ -0,0 +1,19 @@
+package src.sesao23.aula.exercicioDois;
+
+public class Testando {
+    public static void main(String[] args) {
+
+        Caixa <Double> decimais  = new Caixa <>();
+        Caixa <Character> letras  = new Caixa <>();
+        Caixa <String> nome  = new Caixa <>();
+
+        decimais.adicionar(3.0, 0);
+        decimais.adicionar(3.0, 1);
+        letras.adicionar( 'a', 0);
+        nome.adicionar("Ana Beatriz", 0);
+
+
+
+
+    }
+}
